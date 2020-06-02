c7c7383b34a99bd33fde09fe2ea832d5c9b93746
Nicholas Lingrel <nicholas.lingrel@gmail.com>
2020-06-02 04:04:10 -0500

    "Redesign landing page"

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,151 +2,73 @@ import React from 'react'
 
 function Home(props) {
     return (
-        <div className="container-fluid text-center bg-dark text-light">
-            <h3>Welcome to HangarizeSC</h3>
-            <div className="accordion" id="accordionExample">
-                <div className="card text-center bg-secondary text-light">
-                    <div className="card-header text-center" id="headingOne">
-                        <h2 className="mb-0">
-                            <button
-                                className="btn btn-outline-light btn-block text-center"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target="#collapseOne"
-                                aria-expanded="false"
-                                aria-controls="collapseOne"
-                            >
-                                Set up Your Hangar
-                            </button>
-                        </h2>
-                    </div>
-
-                    <div
-                        id="collapseOne"
-                        className="collapse"
-                        aria-labelledby="headingOne"
-                        data-parent="#accordionExample"
-                    >
-                        <div className="card-body">
-                            Build your hangar to match your star citizen account
-                            first, so that your actual hangar can be used for
-                            evaluating potential changes to your fleet.
-                        </div>
-                        <button
-                            className="btn btn-outline-light mb-2"
-                            onClick={props.navToActual}
-                        >
-                            {' '}
-                            Go{' '}
-                        </button>
-                    </div>
-                </div>
-
-                <div className="card text-center bg-secondary text-light">
-                    <div className="card-header text-center" id="headingTwo">
-                        <h2 className="mb-0">
-                            <button
-                                className="btn btn-outline-light btn-block text-center collapsed"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target="#collapseTwo"
-                                aria-expanded="false"
-                                aria-controls="collapseTwo"
-                            >
-                                Set up your Buyback
-                            </button>
-                        </h2>
-                    </div>
-                    <div
-                        id="collapseTwo"
-                        className="collapse"
-                        aria-labelledby="headingTwo"
-                        data-parent="#accordionExample"
-                    >
-                        <div className="card-body">
-                            Build your actual buyback to match your star citizen
-                            account so that your personal buyback options are
-                            available when evaluating potential fleet changes.
-                        </div>
-                        <button
-                            className="btn btn-outline-light mb-2"
-                            onClick={props.navToActual}
-                        >
-                            {' '}
-                            Go{' '}
-                        </button>
-                    </div>
-
-                    <div className="card text-center bg-secondary text-light">
-                        <div
-                            className="card-header text-center"
-                            id="headingThree"
-                        >
-                            <h2 className="mb-0">
-                                <button
-                                    className="btn btn-outline-light btn-block text-center collapsed"
-                                    type="button"
-                                    data-toggle="collapse"
-                                    data-target="#collapseThree"
-                                    aria-expanded="false"
-                                    aria-controls="collapseThree"
-                                >
-                                    Set up your financials
-                                </button>
-                            </h2>
-                        </div>
-                        <div
-                            id="collapseThree"
-                            className="collapse"
-                            aria-labelledby="headingThree"
-                            data-parent="#accordionExample"
-                        >
-                            <div className="card-body">
-                                Enter your account totals to accurately
-                                calculate possible changes using store credit.
+        <div className="container-fluid  bg-dark ">
+            <div className="card bg-dark text-light text-center border-secondary p-0">
+                <div className="card-header">
+                    <h3>Welcome to HangarizeSC</h3>
+                    <div className="card-body bg-secondary text-light">
+                        <div className="card-deck">
+                            <div className="card bg-dark text-center m-2">
+                                <h5>How To Use</h5>
+                                <h6>
+                                    1. Build Your Actual Hangar
+                                    <p>
+                                        An accurate representation of your
+                                        actual hangar as it appears on your Star
+                                        Citizen Account, including accurate
+                                        prices that you paid for each item, is
+                                        needed to get the most out of planning
+                                        changes to your fleet
+                                    </p>
+                                </h6>
+                                <h6>
+                                    2. Use the Hangarizer
+                                    <p>
+                                        Make changes to your hangar. Save these
+                                        changes as a new hangar without
+                                        affecting your actual hangar. You can
+                                        save any number of new hangars with any
+                                        number of differences between them.
+                                    </p>
+                                </h6>
                             </div>
-                            <button
-                                className="btn btn-outline-light mb-2"
-                                onClick={props.navToActual}
-                            >
-                                {' '}
-                                Go{' '}
-                            </button>
+                            <div className="card bg-dark text-center m-2">
+                                <h5>How It Works</h5>
+                                <h6>
+                                    IndexedDB
+                                    <p>
+                                        HangarizeSC saves information in
+                                        IndexedDB, a database controlled by your
+                                        browser. The database and app are
+                                        completely client-side. Your information
+                                        remains on your device and is not
+                                        transmitted to an external server via
+                                        the app. However, this means that your
+                                        hangars are not synced across devices so
+                                        you will have to build hangars on each
+                                        device you want to use. Syncing and data
+                                        back-up are planned optional features
+                                        but are not yet implemented.
+                                    </p>
+                                </h6>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card text-center bg-secondary text-light">
-                    <div className="card-header text-center" id="headingFour">
-                        <h2 className="mb-0">
-                            <button
-                                className="btn btn-outline-light btn-block text-center"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target="#collapseFour"
-                                aria-expanded="true"
-                                aria-controls="collapseFour"
-                            >
-                                Hangarize
-                            </button>
-                        </h2>
-                    </div>
-
-                    <div
-                        id="collapseFour"
-                        className="collapse"
-                        aria-labelledby="headingFour"
-                        data-parent="#accordionExample"
-                    >
-                        <div className="card-body">
-                            Explore potential optimization of your hangar
-                        </div>
-                        <button
-                            className="btn btn-outline-light mb-2"
-                            onClick={props.navToHangarize}
-                        >
-                            {' '}
-                            Go{' '}
-                        </button>
+                    <div className="card-footer text-center bg-dark text-light">
+                        <h6>Disclaimer</h6>
+                        <p>
+                            This app is a work in progress. Do Not Ever use the
+                            app to store private information like your Star
+                            Citizen account credentials. While the data stored
+                            by the app is client-side only, it is persistent and
+                            NOT encrypted and therefore easy to access by anyone
+                            with access to your device. It is not necessary to
+                            enter monetary amounts for the app to function.
+                            Monetary amounts are only used for calculating the
+                            impact of potential fleet changes, eg. whether you
+                            can buy something with available credit or whether
+                            you would have to choose something to melt.
+                        </p>
                     </div>
                 </div>
             </div>
