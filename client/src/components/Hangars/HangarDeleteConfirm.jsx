eb45e497dd0ce71a2c40268c19759b68d52d1a26
Nicholas Lingrel <nicholas.lingrel@gmail.com>
2020-07-07 08:53:36 -0500

    "Style hangar delete confirm"

--- client/src/components/Hangars/HangarDeleteConfirm.jsx
+++ client/src/components/Hangars/HangarDeleteConfirm.jsx
@@ -8,21 +8,35 @@ function HangarDeleteConfirm(props) {
             <div className="input-group-prepend text-light bg-dark mr-2">
                 Delete Hangar?
             </div>
-            <button
-                type="button"
-                className="btn btn-sm btn-success"
-                onClick={props.confirm}
-                value={props.hangarId}
-            >
-                Yes
-            </button>
-            <button
-                type="button"
-                className="btn btn-sm btn-danger"
-                onClick={props.cancel}
-            >
-                No
-            </button>
+            <div className="btn-group">
+                <button
+                    type="button"
+                    className="btn btn-sm btn-success"
+                    onClick={props.confirm}
+                    value={props.hangarId}
+                >
+                    <svg
+                        className="bi bi-check2"
+                        width="1em"
+                        height="1em"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"
+                        />
+                    </svg>
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-sm btn-danger mr-1"
+                    onClick={props.cancel}
+                >
+                    X
+                </button>
+            </div>
         </>
         // {/* </div> */}
         // </div>
